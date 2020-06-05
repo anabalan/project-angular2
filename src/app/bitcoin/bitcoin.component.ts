c93ff3a2870d28cc6131ba35fb5eae2d96dddf37
Ana Carolina <anabalan.b@gmail.com>
2020-06-05 02:45:24 -0300

    :sparkles: Cotação Bitcoin Co-authored-by: Julliana Samora <juusamora@gmail.com>

--- src/app/bitcoin/bitcoin.component.ts
+++ src/app/bitcoin/bitcoin.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+// Importando HTTP Client
+import { HttpClient } from '@angular/common/http';
+
+// Fazendo a tipagem
+interface Bit {
+  time: {
+    updated: string;
+  };
+  bpi: {
+    [key in "USD" | "BRL"]: {
+      code: string;
+      rate: string;
+      rate_float: number;
+    };   
+  };
+};
+
 
 @Component({
   selector: 'app-bitcoin',
   templateUrl: './bitcoin.component.html',
   styleUrls: ['./bitcoin.component.css']
 })
+
 export class BitcoinComponent implements OnInit {
 
-  constructor() { }
+  bitcoins: Bit = null;
 
-  ngOnInit(): void {
+  constructor(private http: HttpClient) { }
+
+  ngOnInit()  {
+    this.update();
   }
+
+  // Chamando git
+  update() {
+    this.http.get<Bit>('https://api.coindesk.com/v1/bpi/currentprice/BRL.json')
+    .subscribe(data => {
+        this.bitcoins = data;
+      })
+    };
 
 }
