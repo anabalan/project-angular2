d62a4e5ce4dd6e38d14ed701309c2048793d1bfa
Ana Carolina <anabalan.b@gmail.com>
2020-06-05 02:36:22 -0300

    :sparkles: Criação de componentes Co-authored-by: Julliana Samora <juusamora@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { BitcoinComponent } from './bitcoin/bitcoin.component';
+import { IndexComponent } from './index/index.component';
+import { MercadoComponent } from './mercado/mercado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: IndexComponent},
+  {path: 'bitcoin', component: BitcoinComponent},
+  {path: 'mercado', component: MercadoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// HTTP Client
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { BitcoinComponent } from './bitcoin/bitcoin.component';
+import { MercadoComponent } from './mercado/mercado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    BitcoinComponent,
+    MercadoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
