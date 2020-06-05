6d81714aa0e9bf0dfe2bc5e3b4b8af19562909ca
Ana Carolina <anabalan.b@gmail.com>
2020-06-05 16:52:53 -0300

    :sparkles: Cotação do bitcoin
    Co-authored-by: Julliana Samora <juusamora@gmail.com>

--- src/app/bitcoin/bitcoin.component.ts
+++ src/app/bitcoin/bitcoin.component.ts
@@ -1,44 +1,74 @@
 import { Component, OnInit } from '@angular/core';
-// Importando HTTP Client
-import { HttpClient } from '@angular/common/http';
 
-// Fazendo a tipagem
-interface Bit {
-  time: {
-    updated: string;
-  };
-  bpi: {
-    [key in "USD" | "BRL"]: {
-      code: string;
-      rate: string;
-      rate_float: number;
-    };   
-  };
-};
-
+import { BitcoinService } from './bitcoin.service';
 
 @Component({
-  selector: 'app-bitcoin',
+  selector: 'app-currency',
   templateUrl: './bitcoin.component.html',
-  styleUrls: ['./bitcoin.component.css']
+  styleUrls: ['./bitcoin.component.css'],
 })
-
 export class BitcoinComponent implements OnInit {
+  constructor(public bitcoinService: BitcoinService) {}
 
-  bitcoins: Bit = null;
-
-  constructor(private http: HttpClient) { }
-
-  ngOnInit()  {
-    this.update();
-  }
-
-  // Chamando git
-  update() {
-    this.http.get<Bit>('https://api.coindesk.com/v1/bpi/currentprice/BRL.json')
-    .subscribe(data => {
-        this.bitcoins = data;
-      })
-    };
-
+  ngOnInit(): void {}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Component, OnInit } from '@angular/core';
+// // Importando HTTP Client
+// import { HttpClient } from '@angular/common/http';
+
+// // Fazendo a tipagem
+// interface Bit {
+//   time: {
+//     updated: string;
+//   };
+//   bpi: {
+//     [key in "USD" | "BRL"]: {
+//       code: string;
+//       rate: string;
+//       rate_float: number;
+//     };   
+//   };
+// };
+
+
+// @Component({
+//   selector: 'app-bitcoin',
+//   templateUrl: './bitcoin.component.html',
+//   styleUrls: ['./bitcoin.component.css']
+// })
+
+// export class BitcoinComponent implements OnInit {
+
+//   bitcoins: Bit = null;
+
+//   constructor(private http: HttpClient) { }
+
+//   ngOnInit()  {
+//     this.update();
+//   }
+
+//  
+//   update() {
+//     this.http.get<Bit>('https://api.coindesk.com/v1/bpi/currentprice/BRL.json')
+//     .subscribe(data => {
+//         this.bitcoins = data;
+//       })
+//     };
+
+// }
--- src/app/bitcoin/bitcoin.service.ts
+++ src/app/bitcoin/bitcoin.service.ts
@@ -1,9 +1,102 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
+import { StorageService } from '../storage.service';
+
+// Tipagem
+type PayloadJSON = {
+  time: {
+    updated: string;
+    updatedISO: string;
+    updateduk: string;
+  };
+  bpi: {
+    [key in 'USD' | 'BRL']: {
+      code: string;
+      rate: string;
+      description: string;
+      rate_float: number;
+    };
+  };
+};
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
+
+
 export class BitcoinService {
 
-  constructor() { }
+  // // Cotação mais atual Dólar
+  // private currentCurrency_usd;
+
+  // // Cotações passadas Dólar
+  // private pastCurrencies_usd = [];
+  // private oldCurrencies_usd = [
+  //   {
+  //     date: '',
+  //     currency: '',
+  //   },
+  // ];
+
+  // // Cotação mais atual Real
+  // private currentCurrency_brl;
+
+  // // Cotações passadas Real
+  // private pastCurrencies_brl = [];
+  // private oldCurrencies_brl = [
+  //   {
+  //     date: '',
+  //     currency: '',
+  //   },
+  // ];
+
+  public indexCurrency;
+  private timer: any;
+
+  private bitcoinData: PayloadJSON;
+  private bitcoinHistory: PayloadJSON[] = [];
+
+  
+  constructor(private http: HttpClient, private storage: StorageService) {
+    this.bitcoinHistory = this.storage.get('@app:bitcoinHistory') ?? [];
+
+    this.requestCurrencyBitcoin();
+    // Repete a função no tempo determinado
+    setInterval(() => {
+      this.requestCurrencyBitcoin();
+    }, 60000);
+  }
+
+  // Cotação atual
+  private requestCurrencyBitcoin() {
+    this.http
+      .get<PayloadJSON>(
+        'https://api.coindesk.com/v1/bpi/currentprice/BRL.json'
+      )
+      .subscribe((data) => {
+        this.bitcoinData = data;
+        this.bitcoinHistory.push(data);
+
+        this.storage.set('@app:bitcoinHistory', this.bitcoinHistory);
+      });
+  }
+  
+  // USD - Acessa o atributo `currentCurrency`
+  getCurrentCurrency_usd() {
+    return this.bitcoinData.bpi.USD.rate_float;
+    
+  }
+
+  // BRL - Acessa o atributo currentCurrency
+  getCurrentCurrency_brl() {
+    return this.bitcoinData.bpi.BRL.rate_float;
+    
+  }
+
+  getBitcoinHistory() {
+    return this.bitcoinHistory;
+  }
+
 }
